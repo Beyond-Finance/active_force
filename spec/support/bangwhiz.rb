6f64b3f13fa5cfa6d6223ecc7b8cefd5f480120f
Billy Frey <87046098+bfrey08@users.noreply.github.com>
2023-05-26 10:07:43 -0600

    Object defaults are not saving to SF DB (#55)
    * add attributes_for_creation and attributes_for_update to the attributes_for_sfdb method
    
    * update specs to include saving to salesforce on defaults
    
    * refactor attributes_for_sfid
    
    * remove shared_context to make specs more readable
    
    * add changelog edits
    
    ---------
    
    Co-authored-by: Billy Frey <wfrey@beyondfinance.com>

--- spec/support/bangwhiz.rb
+++ spec/support/bangwhiz.rb
@@ -1,6 +1,7 @@
 class Bangwhiz < ActiveForce::SObject
 
   field :id,                   from: 'Id'
+  field :name,                 from: 'Name'
   field :percent,              from: 'Percent_Label',  as: :percent, default: -> { 50.0 }
 
 end
