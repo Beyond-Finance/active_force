441f31cdee77e0c49fd02673762b1a4b72cbfad5
Sean Edge <sedge@beyondfinance.com>
2023-11-15 10:14:15 -0500

    Add message for timeout and add specs for scenarios where the job times out.

--- lib/active_force/bulk.rb
+++ lib/active_force/bulk.rb
@@ -4,6 +4,7 @@ require 'active_force/bulk/records'
 module ActiveForce
   module Bulk
     class TimeoutError < Timeout::Error; end
+    TIMEOUT_MESSAGE = 'Bulk job execution expired based on timeout of %{timeout} seconds'.freeze
 
     def bulk_insert_all(attributes, options={})
       run_bulk_job(:insert, attributes, options)
@@ -30,7 +31,7 @@ module ActiveForce
       runtime_options = default_options.merge(options)
       records = Records.parse_from_attributes(translate_to_sf(attributes))
       job = Job.run(operation: operation, object: self.table_name, records: records)
-      Timeout.timeout(runtime_options[:timeout], ActiveForce::Bulk::TimeoutError) do
+      Timeout.timeout(runtime_options[:timeout], ActiveForce::Bulk::TimeoutError, TIMEOUT_MESSAGE % runtime_options) do
         until job.finished? do
           job.info
           sleep(runtime_options[:sleep])
--- spec/active_force/bulk_spec.rb
+++ spec/active_force/bulk_spec.rb
@@ -15,8 +15,8 @@ describe ActiveForce::Bulk do
       {id: 3, baz_id: 2 },
     ]
   end
-
   let(:job) { double(ActiveForce::Bulk::Job) }
+  let(:timeout_message) { /Bulk job execution expired based on timeout of/ }
   before do
     allow(job).to receive(:result)
   end
@@ -25,10 +25,12 @@ describe ActiveForce::Bulk do
     before do
       allow(ActiveForce::Bulk::Job).to receive(:run).with(hash_including(operation: :insert)).and_return(job)
     end
+
     it 'runs a bulk insert job' do
       allow(job).to receive(:finished?).and_return(true)
       subject.bulk_insert_all(attributes)
     end
+
     context 'when job takes a while to run' do
       it 'polls job info until job is finished' do
         allow(job).to receive(:finished?).and_return(false, false, true)
@@ -36,15 +38,28 @@ describe ActiveForce::Bulk do
         subject.bulk_insert_all(attributes)
       end
     end
+
+    context 'when job run exceeds timeout' do
+      it 'raises error' do
+        allow(job).to receive(:finished?).and_return(false)
+        allow(job).to receive(:info)
+        expect do
+          subject.bulk_insert_all(attributes, timeout: 0.1)
+        end.to raise_error(ActiveForce::Bulk::TimeoutError, timeout_message)
+      end
+    end
   end
+
   describe '::bulk_update_all' do
     before do
       allow(ActiveForce::Bulk::Job).to receive(:run).with(hash_including(operation: :update)).and_return(job)
     end
+
     it 'runs a bulk insert job' do
       allow(job).to receive(:finished?).and_return(true)
       subject.bulk_update_all(attributes)
     end
+
     context 'when job takes a while to run' do
       it 'polls job info until job is finished' do
         allow(job).to receive(:finished?).and_return(false, false, true)
@@ -52,20 +67,43 @@ describe ActiveForce::Bulk do
         subject.bulk_update_all(attributes)
       end
     end
+
+    context 'when job run exceeds timeout' do
+      it 'raises error' do
+        allow(job).to receive(:finished?).and_return(false)
+        allow(job).to receive(:info)
+        expect do
+          subject.bulk_update_all(attributes, timeout: 0.1)
+        end.to raise_error(ActiveForce::Bulk::TimeoutError, timeout_message)
+      end
+    end
   end
+
   describe '::bulk_delete_all' do
     before do
       allow(ActiveForce::Bulk::Job).to receive(:run).with(hash_including(operation: :delete)).and_return(job)
     end
+
     it 'runs a bulk insert job' do
       allow(job).to receive(:finished?).and_return(true)
       subject.bulk_delete_all(attributes)
     end
+
     context 'when job takes a while to run' do
       it 'polls job info until job is finished' do
         allow(job).to receive(:finished?).and_return(false, false, true)
         allow(job).to receive(:info)
         subject.bulk_delete_all(attributes)
+      end
+    end
+
+    context 'when job run exceeds timeout' do
+      it 'raises error' do
+        allow(job).to receive(:finished?).and_return(false)
+        allow(job).to receive(:info)
+        expect do
+          subject.bulk_delete_all(attributes, timeout: 0.1)
+        end.to raise_error(ActiveForce::Bulk::TimeoutError, timeout_message)
       end
     end
   end
