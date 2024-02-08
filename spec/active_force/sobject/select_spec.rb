90ebd3543935e7326c26fb943112edaabaf43c40
Billy Frey <wfrey@beyondfinance.com>
2024-02-08 08:50:32 -0700

    add context block to spec

--- spec/active_force/sobject/select_spec.rb
+++ spec/active_force/sobject/select_spec.rb
@@ -9,30 +9,31 @@ module ActiveForce
     end
 
     describe '.select' do
-      it 'query has correct fields' do
+      it 'has correct fields in query' do
         query = Territory.select(:name, :id)
         expect(query.fields).to eq(["Name", "Id"])
       end
 
-      it 'returns SObjects with Uninitialized Value' do
-        response = [build_restforce_sobject({
-          "Id"       => "123",
-          "Quota__c" => "321",
-        })]
-        allow(client).to receive(:query).once.and_return response
-        territory = Territory.select(:id).first
-        expect(territory.instance_variable_get(:@attributes)["name"]).to be_an_instance_of(ActiveModel::Attribute::UninitializedValue)
-      end
+      context 'when getting the value of an uninitialized attribute' do
+        let(:territory) { Territory.select(:id).first }
+        let(:response) do
+          [build_restforce_sobject({
+            "Id"       => "123",
+            "Quota__c" => "321",
+          })]
+        end
 
-      it 'raises missing attribute error if uninitialized variable is called' do
-        response = [build_restforce_sobject({
-          "Id"       => "123",
-          "Quota__c" => "321",
-        })]
-        allow(client).to receive(:query).once.and_return response
-        territory = Territory.select(:id).first
+        before do
+          allow(client).to receive(:query).once.and_return response
+        end
 
-        expect{territory.name}.to raise_error(ActiveModel::MissingAttributeError)
+        it 'raises missing attribute error if uninitialized variable is called' do
+          expect{territory.name}.to raise_error(ActiveModel::MissingAttributeError)
+        end
+
+        it 'returns SObjects with Uninitialized Value' do
+          expect(territory.instance_variable_get(:@attributes)["name"]).to be_an_instance_of(ActiveModel::Attribute::UninitializedValue)
+        end
       end
     end
   end
