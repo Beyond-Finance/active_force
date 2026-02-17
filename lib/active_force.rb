9aa8ef8587daf2b419902bb20700d97d0c888de4
Steven Kim <stevenkim@beyondfinance.com>
2026-02-17 15:11:01 -0600

    Add ability to query using Composite Batch Api

--- lib/active_force.rb
+++ lib/active_force.rb
@@ -9,8 +9,16 @@ module ActiveForce
 
   class << self
     attr_accessor :sfdc_client
+    attr_writer :composite_batch_query_size
   end
 
   self.sfdc_client = Restforce.new
 
+  def self.composite_batch_query_size
+    @composite_batch_query_size ||= 25_000
+
+    return @composite_batch_query_size.call if @composite_batch_query_size.respond_to?(:call)
+
+    @composite_batch_query_size
+  end
 end
--- lib/active_force/active_query.rb
+++ lib/active_force/active_query.rb
@@ -1,6 +1,7 @@
 require 'active_support/all'
 require 'active_force/query'
 require 'active_force/select_builder'
+require 'active_force/composite_batch_query'
 require 'forwardable'
 
 module ActiveForce
@@ -245,7 +246,13 @@ module ActiveForce
     end
 
     def result
-      sfdc_client.query(self.to_s)
+      soql = to_s
+
+      if soql.length >= ActiveForce.composite_batch_query_size
+        CompositeBatchQuery.call(sfdc_client, soql)
+      else
+        sfdc_client.query(soql)
+      end
     end
 
     def build_order_by(args)
